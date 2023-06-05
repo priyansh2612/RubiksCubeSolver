cfb2e977e08eadef37f3ebba5757ccd071d0296a
priyansh2612 <106736812+priyansh2612@users.noreply.github.com>
2023-06-05 18:42:21 +0530

    BFS solver added

---
+++ BFS.h
@@ -0,0 +1,70 @@
+//
+// Created by Dell on 05-06-2023.
+//
+#include<bits/stdc++.h>
+#include "GenCube.h"
+
+
+
+#ifndef RUBIKSCUBESOLVER_BFS_H
+#define RUBIKSCUBESOLVER_BFS_H
+template<typename T, typename H>
+class BFS {
+private:
+    vector<GenCube::MOVE> moves;
+    unordered_map<T, bool, H> visited;
+    unordered_map<T, GenCube::MOVE, H> move_done;
+
+//    bfs() -> performs breadth-first-search and returns a solved Rubik's Cube
+//    move_done[] -> Back-Pointer map as to how we reached that state
+    T bfs() {
+        queue<T> q;
+        q.push(rubiksCube);
+        visited[rubiksCube] = true;
+
+        while (!q.empty()) {
+            T node = q.front();
+            q.pop();
+            if (node.isSolved()) {
+                return node;
+            }
+            for (int i = 0; i < 18; i++) {
+                auto curr_move = GenCube::MOVE(i);
+                node.move(curr_move);
+                if (!visited[node]) {
+                    visited[node] = true;
+                    move_done[node] = curr_move;
+                    q.push(node);
+                }
+                node.invert(curr_move);
+            }
+        }
+        return rubiksCube;
+    }
+
+public:
+    T rubiksCube;
+
+    BFS(T _rubiksCube) {
+        rubiksCube = _rubiksCube;
+    }
+
+//    Performs BFS and returns the vector of moves done to solve the cube
+
+
+    vector<GenCube::MOVE> solve() {
+        T solved_cube = bfs();
+        assert(solved_cube.isSolved());
+        T curr_cube = solved_cube;
+        while (!(curr_cube == rubiksCube)) {
+            GenCube::MOVE curr_move = move_done[curr_cube];
+            moves.push_back(curr_move);
+            curr_cube.invert(curr_move);
+        }
+        rubiksCube = solved_cube;
+        reverse(moves.begin(), moves.end());
+        return moves;
+    }
+};
+
+#endif //RUBIKSCUBESOLVER_BFS_H
