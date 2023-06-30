b040018849cb2b5a6540423fe72e99d75679e1d5
priyansh2612 <106736812+priyansh2612@users.noreply.github.com>
2023-06-30 21:22:21 +0530

    IDAstar solver added

---
+++ PatternDatabases/CornerDBMaker.h
@@ -0,0 +1,23 @@
+
+#ifndef RUBIKSCUBESOLVER_CORNERDBMAKER_H
+#define RUBIKSCUBESOLVER_CORNERDBMAKER_H
+
+#include "CornerPatternDatabase.h"
+#include "../RubiksCubeBitboard.cpp"
+
+using namespace std;
+
+class CornerDBMaker {
+private:
+    string fileName;
+    CornerPatternDatabase cornerDB;
+
+public:
+    CornerDBMaker(string _fileName);
+    CornerDBMaker(string _fileName, uint8_t init_val);
+
+    bool bfsAndStore();
+};
+
+
+#endif //RUBIKSCUBESOLVER_CORNERDBMAKER_H
