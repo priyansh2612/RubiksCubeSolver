75d21212a43e1419938cbe7caae6ebfe77b09161
priyansh2612 <106736812+priyansh2612@users.noreply.github.com>
2023-06-30 18:24:56 +0530

    NibbleArray header added

--- PatternDatabases/NibbleArray.h
+++ PatternDatabases/NibbleArray.h
@@ -1,8 +1,31 @@
-//
-// Created by Dell on 30-06-2023.
-//
+
 
 #ifndef RUBIKSCUBESOLVER_NIBBLEARRAY_H
 #define RUBIKSCUBESOLVER_NIBBLEARRAY_H
+#include <bits/stdc++.h>
 
+using namespace std;
+
+class NibbleArray {
+    size_t size;
+    vector<uint8_t> arr;
+
+public:
+    NibbleArray(const size_t size, const uint8_t val = 0xFF);
+
+    uint8_t get(const size_t pos) const;
+
+    void set(const size_t pos, const uint8_t val);
+
+    unsigned char *data();
+
+    const unsigned char *data() const;
+
+    size_t storageSize() const;
+
+    void inflate(vector<uint8_t> &dest) const;
+
+    void reset(const uint8_t val = 0xFF);
+
+};
 #endif //RUBIKSCUBESOLVER_NIBBLEARRAY_H
